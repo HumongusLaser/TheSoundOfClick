8252870028f9d2276d4ac8e07459577751dba513
Jul0402ia <jul0402ia@gmail.com>
2026-09-23 10:22:09 +0200

    keycup

---
+++ TheSoundOfClick/Model/Keycap.cs
@@ -0,0 +1,32 @@
+﻿namespace TheSoundOfClick
+{
+    public class Keycap : Product
+    {
+        private string theme = "";
+        public string Theme
+        {
+            get { return theme; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentException("Theme of keycap must be at least 2 characters.");
+                }
+                theme = value;
+            }
+        }
+        public bool IsBacklightCompatible { get; set; }
+
+
+        public Keycap(Int128 id, int stock, string name, decimal price, string color, string theme, bool isBacklightCompatible) : base(id, stock, name, price, color)
+        {
+            Theme = theme;
+            IsBacklightCompatible = isBacklightCompatible;
+        }
+
+        public override string GetProductDetails()
+        {
+            return $"Keycap: {Name}, Color: {Color}, Theme: {Theme}, Backlight Compatible: {IsBacklightCompatible}";
+        }
+    }
+}
